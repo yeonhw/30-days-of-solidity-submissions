2edaf344f64a7af4f7ffd6bc5be2f8436106eefc
Moses-main <moseschizaram8@gmail.com>
2025-04-01 21:12:30 +0100

    Day 1 submission

---
+++ submissions/Moses-main/ClickCounter.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.2 <0.9.0;
+
+contract ClickCounter {
+
+    // 📖 Description:
+    // A simple counter contract to learn variable declaration, 
+    // function creation, and basic arithmetic.
+    //  Like a YouTube view counter, tracking how many times a button is clicked.
+
+    uint256 public count;
+
+    // Function to get the current count
+    function get() public view returns (uint256)  {
+        return count;
+    }
+
+    // Funtion to increment count by 1
+    function incr() public {
+        count++;
+    }
+
+    // Function to decrement count by 1
+    function decr() public {
+        require(count > 0,"counter can't be negative");
+        count--;
+    }
+
+
+}
