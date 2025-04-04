2739af5d428c26cf5490985a8cf8ae51975724d9
Moses-main <moseschizaram8@gmail.com>
2025-04-04 03:04:56 +0100

    Day03 - Submission by @Moses-main

--- submissions/Moses-main/day3-contract.sol
+++ submissions/Moses-main/day3-contract.sol
@@ -16,6 +16,7 @@ contract PollStation {
         voteCount[_candidatesNames] = 0;
     }
 
+// 
     function getCandidateNames() public view returns (string[] memory)  {
         return candidateNames;
     }
