63cd1b4644d3c704115e04b4501025fa43661a48
Moses-main <moseschizaram8@gmail.com>
2025-04-14 11:25:31 +0100

    Fix the submission for day 13

--- submissions/Moses-main/day13-contract.sol
+++ submissions/Moses-main/day13-contract.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.0;
+pragma solidity ^0.8.0;
 
 import "./SimpleERC20.sol";
 
-contract SimplifiedTokenSale{
+contract SimplifiedTokenSale is SimpleERC20{
 
     uint256 public tokenPrice;
     uint256 public saleStartTime;
@@ -42,6 +42,72 @@ contract SimplifiedTokenSale{
     }
 
 
+    function isSaleActive() public view returns (bool) {
+        return (!finalized && block.timestamp >= saleStartTime && block.timestamp <= saleEndTime);
+    }
 
+
+      
+    function buyTokens() public payable {
+        require(isSaleActive(), "Sale is not active");
+        require(msg.value >= minPurchase, "Amount is below minimum purchase");
+        require(msg.value <= maxPurchase, "Amount exceeds maximum purchase");
+
+        uint256 tokenAmount = (msg.value * 10**uint256(decimals)) / tokenPrice;
+        require(balanceOf[address(this)] >= tokenAmount, "Not enough tokens left for sale");
+
+        totalRaised += msg.value;
+        _transfer(address(this), msg.sender, tokenAmount);
+        emit TokensPurchased(msg.sender, msg.value, tokenAmount);
+    }
+
+
+      
+    function transfer(address _to, uint256 _value) public override returns (bool) {
+        if (!finalized && msg.sender != address(this) && initialTransferDone) {
+            require(false, "Tokens are locked until sale is finalized");
+        }
+        return super.transfer(_to, _value);
+    }
+
+
+      
+    function transferFrom(address _from, address _to, uint256 _value) public override returns (bool) {
+        if (!finalized && _from != address(this)) {
+            require(false, "Tokens are locked until sale is finalized");
+        }
+        return super.transferFrom(_from, _to, _value);
+    }
+
+
+    function finalizeSale() public payable {
+        require(msg.sender == projectOwner, "Only Owner can call the function");
+        require(!finalized, "Sale already finalized");
+        require(block.timestamp > saleEndTime, "Sale not finished yet");
+
+        finalized = true;
+        uint256 tokensSold = totalSupply - balanceOf[address(this)];
+
+        (bool success, ) = projectOwner.call{value: address(this).balance}("");
+        require(success, "Transfer to project owner failed");
+
+        emit SaleFinalized(totalRaised, tokensSold);
+    }
+
+        
+    function timeRemaining() public view returns (uint256) {
+        if (block.timestamp >= saleEndTime) {
+            return 0;
+        }
+        return saleEndTime - block.timestamp;
+    }
+        
+    function tokensAvailable() public view returns (uint256) {
+        return balanceOf[address(this)];
+    }
+      
+    receive() external payable {
+        buyTokens();
+    }
 
 }
