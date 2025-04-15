1b873f8445cb12d4906a1010a78abc8514b73427
Moses-main <moseschizaram8@gmail.com>
2025-04-16 00:52:18 +0100

    Day15 - Submission by @Moses-main

---
+++ submissions/Moses-main/day15-contract.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+// a simple voting system where users can 
+// vote on proposals. Your challenge is 
+// to make it as gas-efficient as possible. 
+// Optimize how you store voter data, handle 
+// input parameters, and design functions. 
+// You'll learn how calldata, memory, and storage 
+// affect gas usage and discover small changes 
+// that lead to big savings. It's like designing a 
+// voting machine that runs faster and cheaper 
+// without losing accuracy.
+
+
+contract GasSaver{
+
+
+}
