ed6508d765052da2f1949b32072dcc2dd028d00f
Moses-main <moseschizaram8@gmail.com>
2025-04-26 10:38:19 +0100

    Day25 - Submission by @Moses-main

---
+++ submissions/Moses-main/day25-AutomaterMarketMaker/TokenB.sol
@@ -0,0 +1,12 @@
+ 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TokenB is ERC20 {
+    constructor() ERC20("Token B", "TKB") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+}
+
