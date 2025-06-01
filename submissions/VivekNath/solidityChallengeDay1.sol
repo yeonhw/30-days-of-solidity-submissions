a1fa69d6c53bfdad6815d8714581e430d7204154
Vivek nath Goswami <viveknath14701@gmail.com>
2025-06-01 19:31:48 +0530

    solidityChallengeDay1.sol

--- submissions/VivekNath/solidityChallengeDay1.sol
+++ submissions/VivekNath/solidityChallengeDay1.sol
@@ -3,11 +3,17 @@ pragma solidity 0.8.24;
 
 
 contract Counter {
-    uint256 count =0;
+    uint256 public  counter = 0; //The initial value of the counter is 0 
+ 
+function click()public { //This fuction will increse the counter value whenever you click this function
+    counter++;
+}
 
 
+function getCount()public view returns(uint256) { //This function returns the counter value 
+return  counter;
+}
 
-    function click() public {
-        count++;
-    }
+
+    
 }
